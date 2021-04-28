b7f9f18275db1cbacf25d67c3e0ceb1cda6cecb3
Kishinskiy Oleg <out-kishinskiy-ov@sberbank.ru>
2021-04-28 22:49:06 +0300

    use reverse

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 import csv
+import urllib
 
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
@@ -29,10 +30,11 @@ def bus_stations(request):
     current_page = page_obj.number
     prev_page_url=None
     next_page_url=None
+    url = f'{reverse(bus_stations)}?'
     if page_obj.has_previous():
-        prev_page_url = f'?page={page_obj.previous_page_number()}'
+        prev_page_url = url+urllib.parse.urlencode({'page': page_obj.previous_page_number()})
     if page_obj.has_next():
-        next_page_url = f'?page={page_obj.next_page_number()}'
+        next_page_url = url+urllib.parse.urlencode({'page':page_obj.next_page_number()})
 
 
     return render(request, 'index.html', context={
